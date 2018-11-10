a749c4d1d30faa0eb190b86d9d070875f70d8582
esther-j <esther.jang0907@gmail.com>
2018-11-09 23:16:18 -0500

    main function

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((width, height))
+
+running = True
+while running:
+    time = clock.tick(fps) #similar to timerDelay
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
